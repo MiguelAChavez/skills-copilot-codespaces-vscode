f5700182254681ec7e2acc562a0dcddd9497a733
Miguel Angel Chavez <129125500+MiguelAChavez@users.noreply.github.com>
2023-11-23 17:56:04 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,5 +1,26 @@
 //creat web server
-
-
-//1.引入express 
 var express = require('express');
+const router = express.Router();
+const Comment = require('../models/comment');
+
+//handle a get request at path /comments
+router.get('/', (req, res) => {
+    Comment.find()
+        .then((comments) => {
+            res.json(comments);
+        })
+        .catch((err) => {
+            res.status(500).send(err.message);
+        });
+}
+);
+
+router.get('/:id', (req, res) => {
+    Comment.findById(req.params.id)
+        .then((comment) => {
+            res.json(comment);
+        })
+        .catch((err) => {
+            res.status(500).send(err.message);
+        });
+}); 

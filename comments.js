fefec49561c06c992f32c45ee1551757bb315c7a
Miguel Angel Chavez <129125500+MiguelAChavez@users.noreply.github.com>
2023-11-23 17:58:00 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,26 +1,30 @@
 //creat web server
+// comments.js - Express setup and web server creation
 var express = require('express');
 const router = express.Router();
 const Comment = require('../models/comment');
 
-//handle a get request at path /comments
+// Creating an instance of the Express application
+const app = express();
+
+// Middleware, routes, and other configurations could be added here
+
+// Starting the server on a specific port (e.g., 3000)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+// Rest of your comment-related routes and logic would follow below
+// ...
+
+// Sample comment-related routes (as in the provided code)
 router.get('/', (req, res) => {
-    Comment.find()
-        .then((comments) => {
-            res.json(comments);
-        })
-        .catch((err) => {
-            res.status(500).send(err.message);
-        });
-}
-);
+    // Handle GET request logic for comments
+});
 
 router.get('/:id', (req, res) => {
-    Comment.findById(req.params.id)
-        .then((comment) => {
-            res.json(comment);
-        })
-        .catch((err) => {
-            res.status(500).send(err.message);
-        });
-}); 
+    // Handle GET request logic for a specific comment by ID
+});
+
+module.exports = router;

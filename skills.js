2044d867c1ecb93fcdaaf308d46359c9d2d66e61
Miguel Angel Chavez <129125500+MiguelAChavez@users.noreply.github.com>
2023-11-23 17:39:40 +0000

    Copilot second commit

--- skills.js
+++ skills.js
@@ -1,3 +1,36 @@
 function calculateNumbers(var1,  var2) {
     return var1 + var2;
 }
+
+//Obio esto no funciona, pero es para que se entienda como se usa GitHub Copilot para generar codigo
+//tic tac toe
+const board = [ 
+    ['X', 'O', 'X'],
+    ['O', 'X', 'O'],
+    ['O', 'O', 'X']
+];
+
+const turn = Object.freeze({
+    X: 'X',
+    O: 'O'
+});
+
+const Square = (props) => {
+    return (
+        <button className="square" onClick={() => props.onClick(key)}>
+            {props.value}
+        </button>
+    )
+};
+
+const handleClick = (i) => {
+    setSquares(squares[i] = (turn === 'X') ? 'O' : 'X');
+};
+
+function paintBoard(board) {
+   board.map((row) => {
+        row.map((square, index) => {
+            return <Square key={index} value={square} onClick={handleClick} />
+        })
+    })
+}
